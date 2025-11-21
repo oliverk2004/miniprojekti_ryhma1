9283a8fe1d3056bbbc5dbd7c57032f6d9b98a574
Jesse Raatikainen <jesse.v.raatikainen@student.jyu.fi>
2025-11-21 14:58:06 +0200

    Eriytetty funktiot main:sta ja lisätty konsoliluokka helpottamaan testausta

---
+++ src/projekti/funktiot/bibtex_funktiot.py
@@ -0,0 +1,37 @@
+from funktiot.konsoli_IO import KonsoliIO
+
+def listaa_viitteet(bib_tiedosto, io: KonsoliIO):
+    try:
+        with open(bib_tiedosto, "r", encoding="utf-8") as f:
+            content = f.read().strip()
+
+        if not content:
+            io.kirjoita("Ei lähdeviitteitä.\n")
+            return
+        
+        io.kirjoita(f"lähdeviitteet:\n{content}\n")
+    except FileNotFoundError:
+        io.kirjoita(f"Ei lähdeviitteitä, sillä tiedostoa {bib_tiedosto} ei löytynyt.\n")
+
+def lisaa_viite(bib_tiedosto, io: KonsoliIO):
+    io.kirjoita("lisätään uusi lähdeviite.")
+    tyyppi = io.lue("Tyyppi: ")
+    viiteavain = io.lue("Viiteavain: ")
+    tekijä = io.lue("Tekijät: ").strip()
+    otsikko = io.lue("Otsikko: ").strip()
+    vuosi = io.lue("Vuosi: ")
+
+#tähän muut tärkeät tiedot jos esim doi tarvitsee tai muuta
+    
+    bibtex_block = f"@{tyyppi}{{{viiteavain},\n" \
+               f"tekijä = {{{tekijä}}},\n" \
+               f"otsikko = {{{otsikko}}},\n" \
+               f"vuosi = {{{vuosi}}},\n" \
+               f"}}"
+
+#pitäisi luoda bibtexiin viitteen oikean muotoisena
+    
+    with open(bib_tiedosto, "a", encoding="utf-8") as f:
+        f.write("\n\n" + bibtex_block)
+
+    io.kirjoita(f"lähde '{otsikko}' lisätty.\n")
--- src/projekti/main.py
+++ src/projekti/main.py
@@ -1,61 +1,28 @@
-import os
+# import os
+from funktiot.bibtex_funktiot import lisaa_viite, listaa_viitteet
+from funktiot.konsoli_IO import KonsoliIO
+
 BIBFILE = "viitteet.bib"
 
-def listaa_viitteet(bib_tiedosto):
-    try:
-        with open(bib_tiedosto, "r", encoding="utf-8") as f:
-            content = f.read().strip()
-
-        if not content:
-            print("Ei lähdeviitteitä.\n")
-            return
-        
-        print("lähdeviitteet:")
-        print(content)
-    except FileNotFoundError:
-        print(f"Ei lähdeviitteitä, sillä tiedostoa {bib_tiedosto} ei löytynyt.\n")
-
-def lisaa_viite(bib_tiedosto):
-    print("lisätään uusi lähdeviite.")
-    tyyppi = input("Tyyppi: ")
-    viiteavain = input("Viiteavain: ")
-    tekijä = input("Tekijät: ").strip()
-    otsikko = input("Otsikko: ").strip()
-    vuosi = input("Vuosi: ")
-
-#tähän muut tärkeät tiedot jos esim doi tarvitsee tai muuta
-    
-    bibtex_block = f"@{tyyppi}{{{viiteavain},\n" \
-               f"tekijä = {{{tekijä}}},\n" \
-               f"otsikko = {{{otsikko}}},\n" \
-               f"vuosi = {{{vuosi}}},\n" \
-               f"}}"
-
-#pitäisi luoda bibtexiin viitteen oikean muotoisena
-    
-    with open(bib_tiedosto, "a", encoding="utf-8") as f:
-        f.write("\n\n" + bibtex_block)
-
-    print(f"lähde '{otsikko}' lisätty.\n")
-
 def main():
-    print("\nTervetuloa lähdeviite työkaluun!")
+    io = KonsoliIO()
+    io.kirjoita("\nTervetuloa lähdeviite työkaluun!")
     while True:
-        print("Haluatko lisätä lähdeviitteen, listata lähdeviitteesi vai lopettaa?")
+        io.kirjoita("Haluatko lisätä lähdeviitteen, listata lähdeviitteesi vai lopettaa?")
         
-        valitse = input ("> ").strip()
+        valitse = io.lue("> ").strip()
 
         if valitse == "lisää":
-            lisaa_viite(BIBFILE)
+            lisaa_viite(BIBFILE, io)
             continue
         elif valitse == "listaa":
-            listaa_viitteet(BIBFILE)
+            listaa_viitteet(BIBFILE, io)
             continue
         elif valitse == "lopeta":
-            print("Heippa!\n")
+            io.kirjoita("Heippa!\n")
             return
         else:
-            print("en tiedä mitä tarkoitat :(\n")
+            io.kirjoita("en tiedä mitä tarkoitat :(\n")
 
 if __name__ == "__main__":
     main()
