653c9fa5d89a185792acb37bec7662526a3b962e
Oliver Kandén <oliver.kanden04@gmail.com>
2025-11-22 14:22:13 +0200

    Muokkasin testit nyt kurssin sivuilla haluttuun StubIO muotoon

---
+++ src/tests/StubIO.py
@@ -0,0 +1,16 @@
+class StubIO:
+    def __init__(self):
+        self.input = []
+        self.output = []
+
+    def lue(self, teksti=""):
+        self.output.append(teksti)
+        if self.input:
+            return self.input.pop(0)
+        return ""
+
+
+    def kirjoita(self, teksti):
+        return self.output.append(teksti)
+    
+    
