16333fb9dd0025f7b145de597096d5981646b389
Oliver Kandén <oliver.kanden04@gmail.com>
2025-11-22 14:20:15 +0200

    Muokkasin testit nyt kurssin sivuilla haluttuun StubIO muotoon

--- src/tests/test_bibtex_funktiot.py
+++ src/tests/test_bibtex_funktiot.py
@@ -1,5 +1,6 @@
 from projekti.funktiot.bibtex_funktiot import listaa_viitteet, lisaa_viite
 from unittest.mock import Mock
+from tests.StubIO import StubIO
 
 # Ajattelin, että Mock-olioiden hyödyntäminen voisi tässä tapauksessa olla sopiva vaihtoehto...
 # Testi, että konsoli tulostaa "Ei lähdeviitteitä", kun viitteitä ei ole bib-tiedostossa.
@@ -7,10 +8,10 @@ def test_listaa_viitteet_tyhja(tmp_path):
     tiedosto = tmp_path / "test.bib"
     tiedosto.write_text("", encoding = "utf-8")
 
-    io = Mock()
+    io = StubIO()
     listaa_viitteet(tiedosto, io)
 
-    io.kirjoita.assert_called_once_with("Ei lähdeviitteitä.\n")
+    assert io.output[0] == "Ei lähdeviitteitä.\n"
 
 
 # Testi, että konsoli tulostaa viitteet sisältöineen. 
@@ -18,12 +19,11 @@ def test_listaa_viitteet(tmp_path):
     tiedosto = tmp_path / "test.bib"
     tiedosto.write_text("@tyyppi{viiteavain, tekijä={Esimerkki}}", encoding = "utf-8")
 
-    io = Mock()
+    io = StubIO()
     listaa_viitteet(tiedosto, io)
 
-    tuloste = io.kirjoita.call_args[0][0]
-    assert "lähdeviitteet:" in tuloste
-    assert "@tyyppi{viiteavain" in tuloste
+    assert "lähdeviitteet:" in io.output[0]
+    assert "@tyyppi{viiteavain" in io.output[0]
 
 
 # Testi, kun tiedostoa ei löydy
@@ -31,9 +31,7 @@ def test_file_not_found_error(tmp_path):
     tiedosto = tmp_path / "test.bib"
     # Tiedostoa ei ole luotu, koska .write_text puuttuu
 
-    io = Mock()
+    io = StubIO()
     listaa_viitteet(tiedosto, io)
 
-    io.kirjoita.assert_called_once()
-    virheilmoitus = io.kirjoita.call_args[0][0]
-    assert f"Ei lähdeviitteitä, sillä tiedostoa {tiedosto} ei löytynyt." in virheilmoitus
+    assert f"Ei lähdeviitteitä, sillä tiedostoa {tiedosto} ei löytynyt." in io.output[0]
