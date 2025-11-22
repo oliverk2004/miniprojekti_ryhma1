4ba0c01d0bcdae5949e2c36c13da4bf5d3b8e3fa
Oliver Kandén <oliver.kanden04@gmail.com>
2025-11-22 11:18:16 +0200

    Poistin nuo turhat helloworldit

--- src/helloworld.py
+++
@@ -1,7 +0,0 @@
-# TÄMÄN TIEDOSTON SAA POISTAA
-# TEHTY AINOASTAAN TUON CI ACTIONSIA VARTEN ASENNUSVAIHEESSA!
-def helloworld():
-    return "Hello World"
-
-if __name__ == "__main__": 
-    print(helloworld())
--- src/tests/test_helloworld.py
+++
@@ -1,7 +0,0 @@
-import unittest
-from helloworld import helloworld
-
-class TestHelloWorld(unittest.TestCase):
-    def test_hello(self):
-        self.assertEqual(helloworld(), "Hello World")
-
