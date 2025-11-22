92acff9f4b20e6648234bba069e37325a1ab91f5
Oliver Kandén <oliver.kanden04@gmail.com>
2025-11-22 13:32:41 +0200

    toinen testi lisätty

--- src/tests/test_bibtex_funktiot.py
+++ src/tests/test_bibtex_funktiot.py
@@ -1,6 +1,7 @@
 from projekti.funktiot.bibtex_funktiot import listaa_viitteet, lisaa_viite
 from unittest.mock import Mock
 
+# Ajattelin, että Mock-olioiden hyödyntäminen voisi tässä tapauksessa olla sopiva vaihtoehto...
 # Testi, että konsoli tulostaa "Ei lähdeviitteitä", kun viitteitä ei ole bib-tiedostossa.
 def test_listaa_viitteet_tyhja(tmp_path):
     tiedosto = tmp_path / "test.bib"
@@ -10,3 +11,16 @@ def test_listaa_viitteet_tyhja(tmp_path):
     listaa_viitteet(tiedosto, io)
 
     io.kirjoita.assert_called_once_with("Ei lähdeviitteitä.\n")
+
+
+# Testi, että konsoli tulostaa viitteet sisältöineen. 
+def test_listaa_viitteet(tmp_path):
+    tiedosto = tmp_path / "test.bib"
+    tiedosto.write_text("@tyyppi{viiteavain, tekijä={Esimerkki}}", encoding = "utf-8")
+
+    io = Mock()
+    listaa_viitteet(tiedosto, io)
+
+    tuloste = io.kirjoita.call_args[0][0]
+    assert "lähdeviitteet:" in tuloste
+    assert "@tyyppi{viiteavain" in tuloste
