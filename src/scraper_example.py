b0e493bc510cd11092bd006445fac3226e3a1b5c
Letaali <mico.niemi@gmail.com>
2025-12-03 07:41:50 +0200

    parempi scraper

--- src/scraper_example.py
+++ src/scraper_example.py
@@ -5,14 +5,11 @@ def main():
     scraper = Scraper()
 
     #Resolves to https://dl.acm.org/doi/10.1145/2380552.2380613
-    lista = scraper.scrape("10.1145/2380552.2380613") 
-    scraper.quit() 
-    # You can use scrape method on multiple urls, but then always remember to quit after.
-    # Otherwise you leave invisible browsers open and running.
+    title, authors, published = scraper.scrape("10.1145/2380552.2380613") 
 
-    # The presentation is a work-in-progress.
-    for row in lista:
-        print(row)
+    print(title)
+    print(authors)
+    print(published)
     print("end of scraping")
 
 
